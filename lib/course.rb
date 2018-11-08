7745ac843fa49b6b3f7c5bd3f721918a07ad909d
Elana Ryznar <eryzna@gmail.com>
2018-11-08 00:15:40 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -8,9 +8,12 @@ class Course
 
   def self.all
     @@all
+    binding.pry
   end
 
   def self.reset_all
     @@all.clear
   end
+
+  #binding.pry
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,15 +16,18 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
+    #binding.pry
   end
 
   def get_courses
-    self.get_page.css(".post")
+    doc=self.get_page.css(".post")
+    binding.pry
   end
   def make_courses
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
+      #binding.pry
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
